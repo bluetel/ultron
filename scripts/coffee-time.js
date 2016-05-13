e383d5f205378e086447caa9c18998f6752f8bd8
Alex Wilson <a@ax.gy>
2016-05-13 23:15:55 +0100

    Linting fixes

--- scripts/coffee-time.js
+++ scripts/coffee-time.js
@@ -1,9 +1,7 @@
 export default robot => (
-  robot.hear(/\@([\w\.\-]+)/i, (msg) => {
+  robot.hear(/@([\w\.\-]+)/i, (msg) => {
     const user = msg.match[1].trim();
     const users = robot.brain.userForName(user);
-    if (users.length === 1) {
-      return msg.send(`${user} is probably drinking a coffee`);
-    }
+    return (users.length === 1) ? msg.send(`${user} is probably drinking a coffee`) : null;
   })
 );
